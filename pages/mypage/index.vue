bfa15328e5c4396986e5f0e66faa7f9cd9e5c1cd
Dongmin <111101111@naver.com>
2023-06-26 13:27:24 +0900

    마이페이지 - 결제현황 감춤, 결제상세모달 삭제버튼 추가

--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -116,10 +116,11 @@
                                 <img src="/images/board.png" alt="" class="icon" style="width:14px;">
                                 <p class="text">내 마을 명단</p>
                             </nuxt-link>
-                           <nuxt-link to="/infos/approvals" class="menu">
+                            <!-- TODO 230626 감춤 -->
+                           <!-- <nuxt-link to="/infos/approvals" class="menu">
                                 <img src="/images/board.png" alt="" class="icon" style="width:14px;">
                                 <p class="text">결재 요청 현황</p>
-                            </nuxt-link> 
+                            </nuxt-link>  -->
                         </div>
                     </div>
                 </div>
