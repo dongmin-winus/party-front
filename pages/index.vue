3aad81202d2151912ebb7e1eecf45b2f36cd5d15
Dongmin <111101111@naver.com>
2023-07-05 15:53:02 +0900

    배너 슬라이더 추가, 개인랭킹 기준 추가

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
             <div class="fragment" v-if="district && district.id != 0">
                 <section class="section-ad">
                     <swiper :options="bannerOptions">
-                        <swiper-slide v-for="(slide,index) in homeBanner3" :key="slide.id">
+                        <swiper-slide v-for="(slide,index) in homeBanner1" :key="slide.id">
                             <a :href="slide.link_url" target="_blank" class="link">
                                 <img class="img" v-if="slide.image" :src="slide.image.url" alt="-">
                             </a>
@@ -110,7 +110,7 @@
             <div class="fragment" v-else>
                 <section class="section-ad">
                     <swiper :options="bannerOptions">
-                        <swiper-slide v-for="(slide,index) in homeBanner3" :key="slide.id">
+                        <swiper-slide v-for="(slide,index) in homeBanner1" :key="slide.id">
                             <a :href="slide.link_url" target="_blank" class="link">
                                 <img class="img" v-if="slide.image" :src="slide.image.url" alt="-">
                             </a>
@@ -335,11 +335,13 @@
             </section>
 
             <section class="section-ad">
-                <div class="wrap">
-                    <a :href="homeBanner1.link_url" target="_blank" class="link">
-                        <img :src="homeBanner1.image.url" alt="">
-                    </a>
-                </div>
+                <swiper :options="bannerOptions">
+                    <swiper-slide v-for="(slide,index) in homeBanner2" :key="slide.id">
+                        <a :href="slide.link_url" target="_blank" class="link">
+                            <img class="img" v-if="slide.image" :src="slide.image.url" alt="-">
+                        </a>
+                    </swiper-slide>
+                </swiper> 
             </section>
 
             <!-- 우리마을 국회의원 -->
@@ -406,8 +408,40 @@
                             개인랭킹 <span class="point orange">TOP10</span>
                         </div>
                     </div>
-                    <div class="mt-16"></div>
-                    <ul class="m-table type01 rank-container" >
+                    <div class="m-input-checkboxes type01">
+                        <div class="mt-8"></div>
+                        <div class="m-input-checkbox">
+                            <input type="radio" id="11" value="year" v-model="togglePopularList">
+                            <label for="11">전체</label>
+                        </div>
+                        <div class="m-input-checkbox">
+                            <input type="radio" id="12" value="week" v-model="togglePopularList">
+                            <label for="12">주간</label>
+                        </div>
+                        <div class="m-input-checkbox">
+                            <input type="radio" id="13" value="month" v-model="togglePopularList">
+                            <label for="13">월간</label>
+                        </div>
+                        <div class="m-input-checkbox">
+                            <input type="radio" id="14" value="statistics" v-model="togglePopularList">
+                            <label for="14">통계</label>
+                        </div>
+                    </div>
+                    <div class="time-container mt-8">
+                        <div class="left"></div>
+                        <div class="right">
+                            <div class="time">
+                                {{ guideTextPopular }} 기준
+                            </div>
+                            <div class="icon-container" >
+                                <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path opacity="0.2" d="M18.1828 9.87501C18.1828 11.5181 17.6955 13.1243 16.7827 14.4905C15.8698 15.8567 14.5723 16.9215 13.0543 17.5503C11.5363 18.1791 9.86586 18.3436 8.25433 18.0231C6.64279 17.7025 5.1625 16.9113 4.00065 15.7494C2.8388 14.5876 2.04757 13.1073 1.72702 11.4958C1.40646 9.88423 1.57098 8.21383 2.19977 6.6958C2.82856 5.17777 3.89338 3.88028 5.25957 2.96742C6.62576 2.05456 8.23197 1.56732 9.87508 1.56732C12.0784 1.56732 14.1915 2.44259 15.7495 4.00059C17.3075 5.55858 18.1828 7.67168 18.1828 9.87501Z" fill="#777777"/>
+                                    <path d="M9.875 0.875C8.09497 0.875 6.35491 1.40284 4.87487 2.39177C3.39483 3.38071 2.24128 4.78631 1.56009 6.43085C0.8789 8.07538 0.70067 9.88498 1.04794 11.6308C1.3952 13.3766 2.25237 14.9803 3.51104 16.239C4.76971 17.4976 6.37336 18.3548 8.11919 18.7021C9.86502 19.0493 11.6746 18.8711 13.3192 18.1899C14.9637 17.5087 16.3693 16.3552 17.3582 14.8751C18.3472 13.3951 18.875 11.655 18.875 9.875C18.8725 7.48882 17.9235 5.2011 16.2362 3.51382C14.5489 1.82654 12.2612 0.87752 9.875 0.875ZM9.875 17.4904C8.36882 17.4904 6.89646 17.0437 5.64412 16.207C4.39178 15.3702 3.4157 14.1808 2.83931 12.7893C2.26292 11.3977 2.11211 9.86655 2.40595 8.38931C2.69979 6.91207 3.42508 5.55514 4.49011 4.49011C5.55514 3.42508 6.91208 2.69978 8.38931 2.40594C9.86655 2.1121 11.3978 2.26291 12.7893 2.8393C14.1808 3.41569 15.3702 4.39177 16.207 5.64412C17.0438 6.89646 17.4904 8.36882 17.4904 9.875C17.4881 11.894 16.685 13.8297 15.2574 15.2574C13.8297 16.685 11.894 17.4881 9.875 17.4904ZM9.18269 10.5673V5.72115C9.18269 5.53754 9.25563 5.36145 9.38547 5.23162C9.5153 5.10178 9.69139 5.02885 9.875 5.02885C10.0586 5.02885 10.2347 5.10178 10.3645 5.23162C10.4944 5.36145 10.5673 5.53754 10.5673 5.72115V10.5673C10.5673 10.7509 10.4944 10.927 10.3645 11.0568C10.2347 11.1867 10.0586 11.2596 9.875 11.2596C9.69139 11.2596 9.5153 11.1867 9.38547 11.0568C9.25563 10.927 9.18269 10.7509 9.18269 10.5673ZM10.9135 13.6827C10.9135 13.8881 10.8526 14.0889 10.7385 14.2596C10.6243 14.4304 10.4622 14.5635 10.2724 14.6421C10.0826 14.7207 9.87385 14.7413 9.67241 14.7012C9.47097 14.6611 9.28593 14.5622 9.1407 14.417C8.99547 14.2718 8.89656 14.0867 8.85649 13.8853C8.81642 13.6838 8.83699 13.475 8.91559 13.2853C8.99419 13.0955 9.12729 12.9333 9.29806 12.8192C9.46884 12.7051 9.66961 12.6442 9.875 12.6442C10.1504 12.6442 10.4146 12.7536 10.6093 12.9484C10.8041 13.1431 10.9135 13.4073 10.9135 13.6827Z" fill="#777777"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="m-table type01 rank-container mt-32" >
                         <li v-for="(item, index) in popularRankings" :key="index" 
                             :style="getOpacity(index)"
                         >
@@ -438,10 +472,14 @@
             </section>
 
             <div class="mt-12"></div>
-            <section class="section-ad2">
-                <a :href="homeBanner2.link_url" target="_blank" class="link">
-                    <img :src="homeBanner2.image.url" alt="">
-                </a>
+            <section class="section-ad">
+                <swiper :options="bannerOptions">
+                    <swiper-slide v-for="(slide,index) in homeBanner3" :key="slide.id">
+                        <a :href="slide.link_url" target="_blank" class="link">
+                            <img class="img" v-if="slide.image" :src="slide.image.url" alt="-">
+                        </a>
+                    </swiper-slide>
+                </swiper> 
             </section>
 
             <quicks 
@@ -509,7 +547,9 @@ export default {
     data() {
         return {
             toggleList: 'total-rankings',
+            togglePopularList: 'year',
             guideText: '',
+            guideTextPopular: '',
             notOpenChecked:false,
             activateNoticePop: false,
             currentId: 0,
@@ -556,6 +596,7 @@ export default {
                 district_id: "",
                 district: "",
                 rankingUrl: "total-rankings",
+                popularRankUrl: "year"
             },
 
             notices: {
@@ -673,19 +714,19 @@ export default {
             return this.districtRegisterCounts.slice(0,3);
         },
         homeBanner1() {
-            return this.homeBanners.banners.find(banner => {
-                return banner.position === 'home1';
+            return this.homeBanners.banners.filter(banner => {
+                return banner.position === 'top';
             })
         },
         homeBanner2() {
-            return this.homeBanners.banners.find(banner => {
-                return banner.position === 'home2';
+            return this.homeBanners.banners.filter(banner => {
+                return banner.position === 'middle';
             })
         },
         homeBanner3() {
-            const sliderList = [];
-            console.log(this.homeBanners.banners[5], 'check')
-            return [this.homeBanners.banners[5], this.homeBanners.banners[6]]
+            return this.homeBanners.banners.filter(banner => {
+                return banner.position === 'bottom';
+            })
         },
         
         aggregateReviews() {
@@ -907,7 +948,7 @@ export default {
                 });
         },
         async getPopularRakings(count) {
-            const response = await this.$axios.get(`/api/popular-list?limitNumber=${count}`);
+            const response = await this.$axios.get(`/api/popular-list?limitNumber=${count}&date=${this.form.popularRankUrl}`);
             let data = response.data.map((item) => {
                 item.totalCount = this.getPopularTotalCount(item);
                 return item;
@@ -915,7 +956,7 @@ export default {
             data = data.sort((a,b) => {
                 return b.totalCount - a.totalCount;
             })
-            this.popularRankings = data;
+            this.popularRankings = [...data];
         },
 
         //TODO daily-rankings 현재 처리되고 있지 않음
@@ -948,6 +989,22 @@ export default {
                     break;
             }
             this.guideText = guideText;
+        },
+        switchPopularRankGuide(value) {
+            let guideText = '';
+            switch(value) {
+
+                case 'week':
+                guideText = '전주 토요일 00:00 ~ 일요일 23:59';
+                    break;
+                case 'month':
+                guideText = '매월 1회 (전달 1일부터 말일)';
+                    break;
+                case 'year':
+                    guideText = '총 누적';
+                    break;
+            }
+            this.guideTextPopular = guideText;
         },
 
         async updatePosts(districtId) {
@@ -1010,6 +1067,17 @@ export default {
             this.switchRankGuide(value);
             this.getRankings(10);
         },
+        togglePopularList(value) {
+            console.log(value,33333)
+            if(value !== 'statistics') {
+                this.form.popularRankUrl = value;
+                this.switchPopularRankGuide(value);
+                this.getPopularRakings(10);
+            }else if(value === 'statistics') {
+                this.$router.push('/popular/rank')
+            }
+
+        },
 
         congressmanItem: {
             deep: true,
@@ -1029,7 +1097,7 @@ export default {
 
     async mounted() {
         this.switchRankGuide('total-rankings');
-        // await this.updatePosts(this.district.id);
+        this.switchPopularRankGuide('year');
         if(!Cookies.get('allPopClosedToday')) {
             await this.getNoticeContents();
         }
