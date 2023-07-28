9553b18eaa5008218bfa3009f59b2602ace02d19
Dongmin <111101111@naver.com>
2023-07-28 15:36:27 +0900

    메인화면 carousel 플러그인 변경

---
+++ plugins/vue-concise-carousel.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import { Carousel, Slide } from '@jambonn/vue-concise-carousel';
+import '@jambonn/vue-concise-carousel/dist/vue-concise-carousel.css'
+
+Vue.component('carousel', Carousel);
+Vue.component('slide', Slide);
