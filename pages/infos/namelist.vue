e99fe83dd38af3dad6cd0b13ad36115601779712
puridev1155 <92970876+puridev1155@users.noreply.github.com>
2023-12-15 19:04:23 +0900

    Revert "Merge commit '92f18b7ab38d4dc7b79523e883f319335de34aa5'"
    This reverts commit 1ee7ab52d07aa041403770f9e756f5c227a29892, reversing
    changes made to 33e0a1d2bc6ad61e033bb2d726d66c380ba9be79.

--- pages/infos/namelist.vue
+++ pages/infos/namelist.vue
@@ -65,18 +65,10 @@
       </div>  
 
       <div class="mt-12"></div>
-      <div class="flex-w-full bg-lightGray">
-        <div class="col-name">서명일</div> 
-        <div class="col-name">회원정보</div>
-        <div class="col-name" v-if="toggleList == 'confirmed'">발신인원</div>
-      </div>
+      <div class="flex-w-full bg-lightGray"><div class="col-name">서명일</div> <div class="col-name">회원정보</div></div>
       <div class="wrap">
         <div class="m-boards type02">
-            <name v-for="item in items.data" :key="item.id" :item="item" 
-              @clickName="openInfoModal(item)"
-              @clickNumber="openDispatchList(item)"
-              @makeProxyPhoneCall="getProxyPhoneCall(item)"
-            />
+            <name v-for="item in items.data" :key="item.id" :item="item" @click="openInfoModal(item)" @makeProxyPhoneCall="getProxyPhoneCall(item)"/>
         </div>
       </div>
       <scroll-loading @load="loadMore" v-if="items.links.next" />
@@ -91,87 +83,68 @@
     >
       <template #outter>
           <div class="deca">
-            <!-- <div class="m-pop-title" style="padding-top:40px;">
+            <div class="m-pop-title" style="padding-top:40px;">
               회원 정보
-            </div> -->
+            </div>
             <div class="inner-form">
-              <div class="m-input-checkboxes type01">
-                <div class="m-input-checkbox">
-                    <input type="radio" id="info" value="info" v-model="modalTitle">
-                    <label for="info">회원 정보</label>
-                </div>
-                <div class="m-input-checkbox">
-                    <input type="radio" id="dispatchList" value="dispatchList" v-model="modalTitle">
-                    <label for="dispatchList" 
-                    >발신 목록</label>
+              <div class="title">
+                <span class="main">{{ selectedItem.name }}</span>
+                <span class="sub-point">자유마을 회원</span>
+              </div>
+              <div class="mt-8"></div>
+              <div class="date">
+                <p>자유마을 가입일</p>
+                <div class="mt-8"></div>
+
+                <div class="m-input-dates type01">
+                    <div class="m-input-text type01">
+                        <input type="text" v-model="computeDate" readonly>
+                    </div>
                 </div>
               </div>
-              <template v-if="modalTitle == 'info'">
+              <div class="mt-8"></div>
+              <div class="info">
+                <p>정보 관리</p>
                 <div class="mt-8"></div>
-                <div class="title">
-                  <span class="main">{{ selectedItem.name }}</span>
-                  <span class="sub-point">자유마을 회원</span>
-                </div>
-                <div class="mt-8"></div>
-                <div class="date">
-                  <p>자유마을 가입일</p>
-                  <div class="mt-8"></div>
-
-                  <div class="m-input-dates type01">
-                      <div class="m-input-text type01">
-                          <input type="text" v-model="computeDate" readonly>
-                      </div>
+                <div class="m-input-checkboxes type01">
+                  <div class="m-input-checkbox">
+                      <input type="radio" id="12" value="delete" v-model="selectedItem.status">
+                      <label for="12">삭제</label>
+                  </div>
+                  <div class="m-input-checkbox">
+                      <input type="radio" id="13" value="store" v-model="selectedItem.status">
+                      <label for="13" 
+                      :style="selectedItem.status ? '' : 'background-color: #0BAF00; color: #fff'"
+                     >보관</label>
                   </div>
                 </div>
+              </div>
+              <div class="mt-8"></div>
+              <div class="reason" v-if="selectedItem.status == 'delete'">
+                <p>삭제의견</p>
                 <div class="mt-8"></div>
-                <div class="info">
-                  <p>정보 관리</p>
-                  <div class="mt-8"></div>
-                  <div class="m-input-checkboxes type01">
-                    <div class="m-input-checkbox">
-                        <input type="radio" id="12" value="delete" v-model="selectedItem.status">
-                        <label for="12">강성</label>
-                    </div>
-                    <div class="m-input-checkbox">
-                        <input type="radio" id="13" value="store" v-model="selectedItem.status">
-                        <label for="13" 
-                        :style="selectedItem.status ? '' : 'background-color: #0BAF00; color: #fff'"
-                      >활동</label>
-                    </div>
-                  </div>
+                <Dropdown
+                  :menuTitle="'삭제의견'"
+                  :activate="activeDropdown"
+                  :items="reasons"
+                  :selected="reasonSelected"
+                  @toggle="toggleReason"
+                  @change="changeReason"
+                />
+              </div>
+              <div class="mt-8"></div>
+              <div class="comment">
+                <p>의견 메모</p>
+                <div class="m-input-textarea type01 lightgrey">
+                  <textarea placeholder="기타 의견을 입력해주세요" v-model="selectedItem.memo"></textarea>
                 </div>
-                <div class="mt-8"></div>
-                <div class="reason" v-if="selectedItem.status == 'delete'">
-                  <p>삭제의견</p>
-                  <div class="mt-8"></div>
-                  <Dropdown
-                    :menuTitle="'삭제의견'"
-                    :activate="activeDropdown"
-                    :items="reasons"
-                    :selected="reasonSelected"
-                    @toggle="toggleReason"
-                    @change="changeReason"
-                  />
-                </div>
-                <div class="mt-8"></div>
-                <div class="comment">
-                  <p>의견 메모</p>
-                  <div class="m-input-textarea type01 lightgrey">
-                    <textarea placeholder="기타 의견을 입력해주세요" v-model="selectedItem.memo"></textarea>
-                  </div>
-                </div>
-              </template>
-              <template v-else-if="modalTitle == 'dispatchList'">
-                <div v-for="name in selectedItem.manager_name?.split(',')">
-                  <span class="dispatch-name">{{ name }}</span>
-                </div>
-              </template>
+              </div>
             </div>
 
             <div class="buttons">
               <div class="m-btns type01">
                 <div class="m-btn-wrap" >
-                  <button class="m-btn type01 bg-revert-grey  width-100" @click="activeInfoModal = false">닫기</button>
+                  <button class="m-btn type01 bg-revert-grey  width-100" @click="activeInfoModal = false">취소</button>
                 </div>
                 <div class="m-btn-wrap">
                   <button class="m-btn type01 bg-primary width-100" style="color: #fff" @click="confirm">완료</button>
@@ -217,8 +190,6 @@ export default {
       selectedItem: {
       },
       toggleList: 'waiting',
-      
-      modalTitle: 'info',
       formerVn: null,
       word:"",
       activeInfoModal: false,
@@ -340,12 +311,6 @@ export default {
     },
 
     openInfoModal(item) {
-      this.modalTitle = 'info';
-      this.selectedItem = item;
-      this.activeInfoModal = true;
-    },
-    openDispatchList(item) {
-      this.modalTitle = 'dispatchList';
       this.selectedItem = item;
       this.activeInfoModal = true;
     },
@@ -395,7 +360,7 @@ export default {
     display: flex;
     flex-direction: row;
     align-items: center;
-    padding: 0 50px;
+    padding: 0 40px;
   }
   .flex-w-full .col-name {
     width: 40%;
@@ -410,13 +375,13 @@ export default {
   }
   .deca .inner-form {
     width: 90%;
-    height: 80%;
+    height: 70%;
     border-radius: 5px;
     background-color: #fff;
     border: 1px solid #BDC3C7;
 
     position: absolute;
-    top: 5%;
+    top: 15%;
     left: 5%;
   }
   .deca .buttons {
@@ -576,16 +541,6 @@ export default {
 .activated {
   border: 2px solid ; 
   box-shadow: 0 0 10px #0BAF00;
-}
-
-.dispatch-name {
-  display: inline-block;
-  padding: 10px;
-  border-radius: 5px;
-  background-color: #0BAF00;
-  color: #fff;
-  font-size: 22px;
-  margin: 5px 5px;
 }
 
 </style>  
--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -215,10 +215,6 @@
 
                     <div class="menus">
                         <div class="wrap">
-                            <!-- <nuxt-link v-if="staffCertificated == '임원진'" to="/infos/namelist" class="menu">
-                                <img src="@/assets/images/board.png" alt="" class="icon" style="width:14px;">
-                                <p class="text">내 마을 회원</p>
-                            </nuxt-link> -->
                             <nuxt-link to="/mypage/likePosts" class="menu">
                                 <img src="@/assets/images/heart-active.png" alt="" class="icon" style="width:14px;">
                                 <p class="text">좋아요 게시물</p>
@@ -263,6 +259,10 @@
 
                     <div class="menus">
                         <div class="wrap">
+                            <nuxt-link v-if="staffCertificated == '임원진'" to="/infos/namelist" class="menu">
+                                <img src="@/assets/images/board.png" alt="" class="icon" style="width:14px;">
+                                <p class="text">내 마을 회원</p>
+                            </nuxt-link>
                             <nuxt-link to="/mypage/likePosts" class="menu">
                                 <img src="@/assets/images/heart-active.png" alt="" class="icon" style="width:14px;">
                                 <p class="text">좋아요 게시물</p>
